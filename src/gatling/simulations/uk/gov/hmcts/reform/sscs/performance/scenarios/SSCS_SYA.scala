afe2eb5e06935a0cf2092eb189586e6a204ef960
Nickin Sitaram <nickin.sitaram@hmcts.net>
2021-02-11 23:33:31 +0000

    Increased coverage

--- src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/scenarios/SSCS_SYA.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/scenarios/SSCS_SYA.scala
@@ -10,6 +10,7 @@ object SSCS_SYA
 {
 
   val BaseURL = Environment.baseURL
+  val IdAMURL = Environment.idamURL
 
   val MinThinkTime = Environment.minThinkTime
   val MaxThinkTime = Environment.maxThinkTime
@@ -24,8 +25,9 @@ object SSCS_SYA
 
   // Launch Homepage
 
-  group("SSCS_010_Homepage") {
-      exec(http("Load SSCS Homepage")
+  group("SSCS_010_Homepage") 
+  {
+      exec(http("Homepage")
           .get(BaseURL + "/")
           .headers(CommonHeader)
           .check(substring("Select a benefit type")))
@@ -35,14 +37,247 @@ object SSCS_SYA
 
   // Enter PIP as Benefit Type 
   
-  .group("SSCS_020_SelectBenefitType") {
-      exec(http("Enter PIP As Benefit")
+  .group("SSCS_020_SelectBenefitType") 
+  {
+      exec(http("PIP Benefit")
           .post(BaseURL + "/benefit-type")
-          .headers(CommonHeader) // additional
-          .headers(PostHeader) // define these in env
+          .headers(CommonHeader) 
+          .headers(PostHeader) 
           .formParam("benefitType", "Personal Independence Payment (PIP)")
           .check(substring("What language do you want us to use when")))
   }
 
   .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+  // Select English as Language
+
+  .group("SSCS_020_SelectLanguage")
+  {
+    exec(http("Language")
+        .post(BaseURL + "/language-preference")
+        .headers(CommonHeader)
+        .headers(PostHeader) 
+        .formParam("languagePreferenceWelsh", "no")
+        .check(substring("Enter your postcode")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter Post Code
+
+  .group("SSCS_030_EnterPostCode")
+  {
+    exec(http("Post Code")
+        .post(BaseURL + "/postcode-check")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("postcode", "TS1 1ST")
+        .check(substring("Your appeal will be reviewed by a tribunal")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Click Continue on appeal will be decided by an independent tribunal
+
+  .group("SSCS_040_DecidedIndependent")
+  {
+    exec(http("Decied Independent Tribunal")
+        .post(BaseURL + "/independence")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .check(substring("I want to be able to save this appeal later")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Select I want to be able to save this appeal later
+
+  .group("SSCS_050_SaveAppealLater")
+  {
+    exec(http("Save Appeal For Later")
+        .post(BaseURL + "/create-account")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("createAccount", "yes")
+        .check(regex("""state=(.*)" method""").saveAs("stateId"))
+        .check(regex("""name="_csrf" value="(.*)""").saveAs("csrf"))
+        .check(substring("Sign in or create an account")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+
+// Enter Login credentials
+
+  .group("SSCS_060_SignIn")
+  {
+    exec(http("Sign In")
+        .post(IdAMURL + "/login?client_id=sscs&redirect_uri=https%3A%2F%2Fbenefit-appeal.perftest.platform.hmcts.net%2Fauthenticated&ui_locales=en&response_type=code&state=$(stateId)")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("username", "sscs+myapt.182@mailinator.com")
+        .formParam("password", "Pass19word")
+        .formParam("save", "Sign in")
+        .formParam("selfRegistrationEnabled", "true")
+        .formParam("_csrf", "${csrf}")
+        .check(substring("Your draft benefit appeals")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Click Create New application
+
+  .group("SSCS_070_NewApplication")
+  {
+    exec(http("Create New Application")
+        .get(BaseURL + "/new-appeal")
+        .headers(CommonHeader) 
+        .check(substring("Select a benefit type")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+/*
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+
+// Enter 
+
+  .group("SSCS_")
+  {
+    exec(http("")
+        .post(BaseURL + "")
+        .headers(CommonHeader) 
+        .headers(PostHeader) 
+        .formParam("")
+        .check(substring("")))
+  }
+
+  .pause(MinThinkTime seconds,MaxThinkTime seconds)
+*/
+
+
+
 }
